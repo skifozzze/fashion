f3dbd7c92d94b93b8577599cac52fb835321c2b7
skifozzze <skifozzze@gmail.com>
2022-01-06 22:10:16 +0200

    added home page

--- #src/js/rateyo.js
+++ #src/js/rateyo.js
@@ -1,11 +1,12 @@
 $(function () {
  
   $(".home-right__response-star").rateYo({
-    ratedFill: "rgba(255, 192, 70, 1)",   
-    readOnly: true,
-    starWidth: "14px",
+    ratedFill: "#F07F11",  
+    normalFill: "#da5d1a3b",    
+    readOnly: true,  
+    starWidth: "16px",
     spacing: "5px", 
-    starSvg: '<svg width="14" height="12" viewBox=""0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.63539 0.085022L8.68572 4.23878L13.2708 4.90895L9.95308 8.14038L10.7361 12.7055L6.63539 10.549L2.53472 12.7055L3.31769 8.14038L0 4.90895L4.58505 4.23878L6.63539 0.085022Z" fill="#F07F11"/></svg> '    
+    starSvg: '<svg width="14" height="13" viewBox=""0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.63539 0.085022L8.68572 4.23878L13.2708 4.90895L9.95308 8.14038L10.7361 12.7055L6.63539 10.549L2.53472 12.7055L3.31769 8.14038L0 4.90895L4.58505 4.23878L6.63539 0.085022Z"/></svg> '    
   });
  
 });
